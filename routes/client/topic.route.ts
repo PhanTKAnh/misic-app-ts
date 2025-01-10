7385cf25b2fbd6a1bdd7ad9c3d3afca4f92060be
PhanTKAnh <146057360+PhanTKAnh@users.noreply.github.com>
2025-01-10 16:07:10 +0700

    tao controller

--- routes/client/topic.route.ts
+++ routes/client/topic.route.ts
@@ -2,17 +2,9 @@
 import { Request, Response, Router } from "express";
 const router: Router = Router();
 
-import Topic from "../../models/topics.model";
+import * as controller from "../../controllers/client/topic.controller"
 
 
-router.get("/", async (req:Request, res: Response) =>{
-    const topics = await Topic.find({
-        deleted:false
-    });
-
-    console.log(topics);
-    
-    res.render("client/pages/topics/index");
-});
+router.get("/", controller.topics );
 
 export const topicsRoutes: Router = router;
