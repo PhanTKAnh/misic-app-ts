17379c5482c1941cb83689040777cf9ac9f63ab2
PhanTKAnh <146057360+PhanTKAnh@users.noreply.github.com>
2025-01-10 15:53:35 +0700

    lay ra achu de bai hat

---
+++ models/topics.model.ts
@@ -0,0 +1,21 @@
+import mongoose from "mongoose"
+const topicSchema = new mongoose.Schema(
+    {
+        title:String,
+        avatar:String,
+        description:String,
+        status: String,
+        slug: String,
+        deleted: {
+            type: Boolean,
+            default: false
+        },
+        deleteAt: Date
+
+    }, {
+    timestamps: true
+}
+);
+const Topic = mongoose.model('Topic', topicSchema, "topic");
+
+export default Topic;
