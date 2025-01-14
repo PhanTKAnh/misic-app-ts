8c81cb6a158e375574395a8144442e1a698aaffc
PhanTKAnh <146057360+PhanTKAnh@users.noreply.github.com>
2025-01-14 22:53:23 +0700

    list music

---
+++ models/song.model.ts
@@ -0,0 +1,26 @@
+import mongoose from "mongoose"
+const songSchema = new mongoose.Schema(
+    {
+       title:String,
+        avatar:String, 
+        description:String,
+        singerId: String,
+        topicId: String,
+        like: Number,
+        lyrics:String,
+        audio: String,
+        status: String,
+        slug: String,
+        deleted: {
+            type: Boolean,
+            default: false
+        },
+        deleteAt: Date
+
+    }, {
+    timestamps: true
+}
+);
+const Song = mongoose.model('Song', songSchema, "song");
+
+export default Song;
--- routes/client/index.route.ts
+++ routes/client/index.route.ts
@@ -1,9 +1,11 @@
 import { Express } from "express";
 import { topicsRoutes } from "./topic.route";
+import { songRoutes } from "./song.route";
 
 const clientRoutes = (app: Express): void =>{
 
     app.use(`/topics`, topicsRoutes)
+    app.use(`/songs`, songRoutes)
 }
 
 export default clientRoutes;
