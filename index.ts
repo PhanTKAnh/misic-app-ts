538e8d9e1f58a17d3dcfbefdb85588e26951878c
PhanTKAnh <146057360+PhanTKAnh@users.noreply.github.com>
2025-02-06 23:54:44 +0700

    create folder dist

--- index.ts
+++ index.ts
@@ -16,14 +16,13 @@ database.connect();
 const app: Express = express();
 const port:string | number = process.env.PORT || 3000;
 
-app.use(express.static("public"))
-
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(methodOverride('_method'))
+app.set('views', `${__dirname}/views`);
+app.set("view engine", "pug");
 
-app.set("views","./views");
-app.set("view engine","pug");
+app.use(express.static(`${__dirname}/public`));
 
 // App Local Variables
 app.locals.prefixAdmin = systemConfig.prefixAdmin;
